19fe01a82019d61a980027d76e294e4537edd926
Jeff Knox <knoxjeffrey@outlook.com>
2015-04-17 10:45:57 +0100

    friends in group

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -16,5 +16,21 @@ describe Group do
   
   it { should validate_length_of(:group_name).is_at_least(3) } 
   
+  describe :friends_in_group do
+    
+    let!(:current_user) { object_generator(:user) }
+    let!(:friend_user1) { object_generator(:user) }
+    let!(:friend_user2) { object_generator(:user) }
+    let!(:group) { object_generator(:group) }
+    let!(:group_member1) { object_generator(:user_group, user: current_user, group: group) }
+    let!(:group_member2) { object_generator(:user_group, user: friend_user1, group: group) }
+    let!(:group_member3) { object_generator(:user_group, user: friend_user2, group: group) }
+    
+    it "returns a list of friends in the current users group" do
+      expect(group.friends_in_group(current_user)).to match([friend_user1, friend_user2])
+    end
+
+  end
+  
 end
   
