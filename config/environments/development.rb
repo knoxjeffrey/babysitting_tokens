96b889cfedc45eed0e9bb45aa1bc8dad1663bf57
Jeff Knox <knoxjeffrey@outlook.com>
2015-04-21 14:59:03 +0100

    change production env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,21 +1,23 @@
 BabysittingTokens::Application.configure do
+  config.cache_classes = false
 
-  config.cache_classes = true
-  config.eager_load = true
-  config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  # Show full error reports and disable caching
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = false
 
-  config.serve_static_assets = false
+  # Don't care if the mailer can't send
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
-  config.assets.compress = true
-  config.assets.js_compressor = :uglifier
+  # Print deprecation notices to the Rails logger
+  config.active_support.deprecation = :log
 
-  config.assets.compile = false
+  # Do not compress assets
+  config.assets.compress = false
 
-  config.assets.digest = true
+  # Expands the lines which load the assets
+  config.assets.debug = true
 
-  config.i18n.fallbacks = true
-
-  config.active_support.deprecation = :notify
-  
+  config.eager_load = false
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,23 +1,21 @@
 BabysittingTokens::Application.configure do
-  config.cache_classes = false
 
-  # Show full error reports and disable caching
-  config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.cache_classes = true
+  config.eager_load = true
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
 
-  # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.serve_static_assets = false
 
-  # Print deprecation notices to the Rails logger
-  config.active_support.deprecation = :log
+  config.assets.compress = true
+  config.assets.js_compressor = :uglifier
 
-  # Do not compress assets
-  config.assets.compress = false
+  config.assets.compile = false
 
-  # Expands the lines which load the assets
-  config.assets.debug = true
+  config.assets.digest = true
 
-  config.eager_load = false
+  config.i18n.fallbacks = true
+
+  config.active_support.deprecation = :notify
+  
 end
