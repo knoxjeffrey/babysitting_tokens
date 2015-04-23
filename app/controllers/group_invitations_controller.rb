fd484bafc24e9671f35edd51662d8946e7c88f36
Jeff Knox <knoxjeffrey@outlook.com>
2015-04-23 16:06:12 +0100

    create new group invitation and tests complete

--- app/controllers/group_invitations_controller.rb
+++ app/controllers/group_invitations_controller.rb
@@ -3,11 +3,24 @@ class GroupInvitationsController < ApplicationController
   before_action :require_user
   
   def new
-    @invitation = GroupInvitation.new
+    @group_invitation = GroupInvitation.new
   end
 
   def create
+    @group_invitation = GroupInvitation.new((invitation_params).merge!(inviter_id: current_user.id, group_id: params[:id]))
+    if @group_invitation.save
+      #AppMailer.delay.send_invite(@invitation)
+      flash[:success] = "You have successfully sent an invitaton to #{@group_invitation.friend_email}"
+      redirect_to group_path(params[:id])
+    else
+      render :new
+    end
+  end
   
+  private
+  
+  def invitation_params
+    params.require(:group_invitation).permit(:friend_name, :friend_email, :message)
   end
   
 end
--- spec/controllers/group_invitations_controller_spec.rb
+++ spec/controllers/group_invitations_controller_spec.rb
@@ -11,21 +11,90 @@ describe GroupInvitationsController do
       let!(:group) { object_generator(:group, admin: current_user) }
       let!(:group_member) { object_generator(:user_group, user: current_user, group: group) }
       
-      it "assigns @invitation" do
+      it "assigns @group_invitation" do
         get :new, id: group.id
         
-        expect(assigns(:invitation)).to be_new_record
-        expect(assigns(:invitation)).to be_instance_of(GroupInvitation)
+        expect(assigns(:group_invitation)).to be_new_record
+        expect(assigns(:group_invitation)).to be_instance_of(GroupInvitation)
       end
     end
     
     context "with unauthenticated user" do
       
-      let!(:group) { object_generator(:group, admin: current_user) }
-      let!(:group_member) { object_generator(:user_group, user: current_user, group: group) }
+      let!(:user) { object_generator(:user) }
+      let!(:group) { object_generator(:group, admin: user) }
+      let!(:group_member) { object_generator(:user_group, user: user, group: group) }
       
       it_behaves_like "require_sign_in" do
         let(:action) { get :new, id: group.id }
+      end
+    end
+  end
+  
+  describe "POST create" do
+    
+    context "with authenticated user" do
+      
+      before { set_current_user_session }
+      #after { ActionMailer::Base.deliveries.clear }
+      
+      context "with valid input" do
+        
+        let!(:group) { object_generator(:group, admin: current_user) }
+        let!(:group_member) { object_generator(:user_group, user: current_user, group: group) }
+        
+        before { post :create, id: group.id, group_invitation: generate_attributes_for(:group_invitation, friend_name: "Test Friend", friend_email: "test@test.com") }
+        
+        it "redirects to the invite friend page" do
+          expect(response).to redirect_to group_path(group.id)
+        end
+        
+        it "creates a group invitation" do
+          expect(GroupInvitation.count).to eq(1)
+        end
+        
+        it "sends an email to the invited friend" do
+          #expect(ActionMailer::Base.deliveries.last.to).to eq(['test@test.com'])
+        end
+        
+        it "generates a successful flash message" do
+          expect(flash[:success]).to be_present
+        end
+      end
+      
+      context "with invalid input" do
+        
+        let!(:group) { object_generator(:group, admin: current_user) }
+        let!(:group_member) { object_generator(:user_group, user: current_user, group: group) }
+        
+        before { post :create, id: group.id, group_invitation: generate_attributes_for(:group_invitation, friend_name: "", friend_email: "test@test.com") }
+        
+        it "does not create a new invitation" do
+          expect(GroupInvitation.count).to eq(0)
+        end
+        
+        it "does not send out an invitation email" do
+          #expect(ActionMailer::Base.deliveries).to be_empty
+        end
+  
+        it "renders the new template" do
+          expect(response).to render_template :new
+        end
+        
+        it "assigns @group_invitation" do
+          expect(assigns(:group_invitation)).to be_present
+        end
+      end
+    end
+    
+    context "with unauthenticated user" do
+      
+      let!(:user) { object_generator(:user) }
+      let!(:group) { object_generator(:group, admin: user) }
+      let!(:group_member) { object_generator(:user_group, user: user, group: group) }
+      
+      it_behaves_like "require_sign_in" do
+        let(:action) { post :create, id: group.id }
       end
     end
   end
