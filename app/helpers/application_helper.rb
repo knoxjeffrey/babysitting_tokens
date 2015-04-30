29c4ba2491e3acd9cb7a2c60dc00677070c248ca
Jeff Knox <knoxjeffrey@outlook.com>
2015-04-30 14:48:01 +0100

    use facebook profile pic if available

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,9 +23,14 @@ module ApplicationHelper
   end
   
   def avatar_url(email, size)
-    gravatar_id = Digest::MD5::hexdigest(email).downcase
-    default_url = "https://s3-eu-west-1.amazonaws.com/babysitting-tokens-development/UniversalImages/baby_avatar.png"
-    "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=#{CGI.escape(default_url)}"
+    user = User.find_by(email: email)
+    if !!user.authentications
+      "http://graph.facebook.com/#{user.authentications.last.uid}/picture?type=normal"
+    else
+      gravatar_id = Digest::MD5::hexdigest(email).downcase
+      default_url = "https://s3-eu-west-1.amazonaws.com/babysitting-tokens-development/UniversalImages/baby_avatar.png"
+      "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=#{CGI.escape(default_url)}"
+    end
   end
   
 end
