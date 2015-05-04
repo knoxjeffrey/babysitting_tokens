ddc964eae330ae646564963b048da08dfe0d4c3d
Jeff Knox <knoxjeffrey@outlook.com>
2015-05-04 11:31:35 +0100

    refactor forgot password controller

--- app/controllers/forgot_passwords_controller.rb
+++ app/controllers/forgot_passwords_controller.rb
@@ -10,17 +10,22 @@ class ForgotPasswordsController < ApplicationController
       MyMailer.delay.send_forgot_password(user)
       redirect_to forgot_password_confirmation_path
     else
-      if params[:email].blank?
-        flash[:danger] = "Email cannot be blank. Please try again."
-      else
-        flash[:danger] = "If this is a valid email address you will receive instructions to reset your password in a few minutes.  
-                          If you do not receive an email then please try again."
-      end
-      
+      check_email_params
       redirect_to forgot_password_path
     end
   end
   
   def confirm; end
+  
+  private
+  
+  def check_email_params
+    if params[:email].blank?
+      flash[:danger] = "Email cannot be blank. Please try again."
+    else
+      flash[:danger] = "If this is a valid email address you will receive instructions to reset your password in a few minutes.  
+                        If you do not receive an email then please try again."
+    end
+  end
   
 end
