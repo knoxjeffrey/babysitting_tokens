6a138bf60590d59337fd95b32b06cf5b9a97a5d9
Jeff Knox <knoxjeffrey@outlook.com>
2015-05-04 10:04:11 +0100

    request button spec

--- spec/features/request_button_spec.rb
+++ spec/features/request_button_spec.rb
@@ -2,9 +2,16 @@ require 'spec_helper'
 
 feature "request button" do
   
-  scenario "new user without invite shouldn't see request button" do
+  scenario "new user without a group shouldn't see request button" do
     new_user_signs_up
     new_user_signs_in
+  end
+  
+  scenario "any user that is a member of a group should see request button" do
+    new_user_signs_up
+    new_user_signs_in
+    create_new_group
+    expect(page).to have_content("Request For Freedom")
   end
   
   def new_user_signs_up
