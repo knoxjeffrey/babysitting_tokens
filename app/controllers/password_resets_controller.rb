a3ce029d7a7e82fac7d7c4366d179ea896b00e75
Jeff Knox <knoxjeffrey@outlook.com>
2015-05-04 11:39:51 +0100

    refactor password reset controller

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -14,18 +14,23 @@ class PasswordResetsController < ApplicationController
     user = User.where(password_token: params[:password_token]).first
     if user
       user.password = params[:password]
-      
-      if user.save
-        user.remove_password_token!
-        flash[:success] = "Your password has been changed, please sign in"
-        redirect_to sign_in_path
-      else
-        @user = user
-        @password_token = params[:password_token]
-        render :show
-      end
+      save_new_password(user)
     else
       redirect_to expired_password_token_path
+    end
+  end
+  
+  private
+  
+  def save_new_password(user)
+    if user.save
+      user.remove_password_token!
+      flash[:success] = "Your password has been changed, please sign in"
+      redirect_to sign_in_path
+    else
+      @user = user
+      @password_token = params[:password_token]
+      render :show
     end
   end
   
